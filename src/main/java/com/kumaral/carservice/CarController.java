82389b4081e998301e9a3800870a900b42ef1016
alok.kumar@sincrodigital.com <alok.kumar@sincrodigital.com>
2022-02-16 13:01:01 +0530

    added one request mapping for testing

--- src/main/java/com/kumaral/carservice/CarController.java
+++ src/main/java/com/kumaral/carservice/CarController.java
@@ -4,6 +4,7 @@ import com.kumaral.carservice.model.Car;
 import com.kumaral.carservice.persistence.CarRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +24,11 @@ public class CarController {
     @GetMapping("/cars")
     public List<Car> getCars() {
         return carRepository.findAll();
+    }
+
+    @GetMapping("/cars/{name}")
+    public Car getCarByName(@PathVariable final String name) {
+        return getCars().stream().filter(c -> c.getName().equals(name)).findFirst().get();
     }
 
     @GetMapping("/msg")
